6a4d889aed24a74350042783377c6e8acff3eb7e
Ximo Pechuan i Jorge <lionzord@gmail.com>
2023-01-25 12:29:12 -0800

    Update RunVelocyto.sh

--- brd9/pipelines/RunVelocyto.sh
+++ brd9/pipelines/RunVelocyto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -q  long          # submit to long queue
-#SBATCH -J  CellBender    # job name
+#SBATCH -J  Velocyto    # job name
 #SBATCH -p  himem #highmemory nodes
 
-velocyto run10x /gstore/scratch/u/pechuanj/NGS3422/CellRanger/LIB5435059_SAM24392038 /gne/data/dnaseq/analysis/aplle/genomes/refdata-cellranger-igis-4.0/GRCm38/genes/genes.gtf
+velocyto run10x /path/to/CellRanger/LIB5435059_SAM24392038 GRCm38/genes/genes.gtf
