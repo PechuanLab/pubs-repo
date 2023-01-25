4fb20b034963ff24ef5fc6e5aa7632fc8c322aa4
Ximo Pechuan i Jorge <lionzord@gmail.com>
2023-01-25 11:27:11 -0800

    Create CellBender.sh

---
+++ brd9/pipelines/CellBender.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -q  long          # submit to long queue
+#SBATCH -J  CellBender    # job name
+#SBATCH -p  himem #highmemory nodes
+
+cellbender remove-background \
+     --input /path/to/cellranger/LIB/outs \
+     --output ./LIB.h5 \
+     --expected-cells 5491 \
+     --total-droplets-included 15000
